e0af5ffb4ae8b6dae941465015e1a246bc3d08dc
Or Antebi <or87600@gmail.com>
2025-03-04 17:55:18 +0200

    Change NoteTodos styles and add onClick

--- apps/note/cmps/NoteType/NoteTodos.jsx
+++ apps/note/cmps/NoteType/NoteTodos.jsx
@@ -1,20 +1,54 @@
-const { Fragment } = React
+import { noteService } from "../../services/note.service.js"
+
+const { useState } = React
+
 
 export function NoteTodos({ note }) {
+    const [todos, setTodos] = useState(note.info.todos)
 
-    const { title, todos } = note.info
+    function toggleTodo(idx) {
+        const updatedTodos = todos.map((todo, i) => {
+            if (i === idx) {
+                return { ...todo, doneAt: todo.doneAt ? null : Date.now() }
+            }
+            return todo
+        })
+        setTodos(updatedTodos)
+        saveUpdatedTodos(updatedTodos)
+    }
+
+    function saveUpdatedTodos(updatedTodos) {
+        const updatedNote = {
+            ...note,
+            info: {
+                ...note.info,
+                todos: updatedTodos
+            }
+        }
+        noteService.save(updatedNote)
+            .catch(err => {
+                console.error('Failed to save the updated todos', err)
+            })
+    }
 
     return (
-        <Fragment>
-            <h3>{title}</h3>
+        <div>
+            <h3>{note.info.title}</h3>
             <ul className="clean-list">
                 {todos.map((todo, idx) => (
-                    <li key={idx}>
-                        <input type="checkbox" checked={!!todo.doneAt} />
-                        <span>{todo.txt}</span>
+                    <li key={idx} className="todo-item">
+                        <label className="todo-label">
+                            <input
+                                type="checkbox"
+                                className="todo-checkbox"
+                                checked={!!todo.doneAt}
+                                onChange={() => toggleTodo(idx)}
+                            />
+                            <span className="todo-text">{todo.txt}</span>
+                        </label>
                     </li>
                 ))}
             </ul>
-        </Fragment>
+        </div>
     )
 }
